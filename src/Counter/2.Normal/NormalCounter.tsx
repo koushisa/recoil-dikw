bf7f4c9fc6421240069aafe8a297642efa6c41a5
Shinya Komatsu <31304738+koushisa@users.noreply.github.com>
2022-07-08 15:29:37 +0900

    Update NormalCounter.tsx

--- src/Counter/2.Normal/NormalCounter.tsx
+++ src/Counter/2.Normal/NormalCounter.tsx
@@ -1,6 +1,7 @@
 import { CounterPresenter } from "../Counter.presenter";
 import { useCounter, useUpdateCounter } from "./normalCounterState";
 
+// コンポーネントはプロダクトの知恵(=ユーザー体験)を表現する
 export const NormalCounter: React.FC = () => {
   const { counter, multiplied } = useCounter();
   const { increment, incrementAsync } = useUpdateCounter();
